6055790a8278e578f6d06a1ce0a598f95f44c4ce
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-03-02 20:18:06 -0500

    Added map to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Leaflet from "./ui/map";
 
 export function Footer() {
   return (
     <Flex height={"40vh"} width="100%" justify={"space-evenly"} pt={"16"} align={"center"} justifyContent={"center"}>
-      <Box>
+      <Box width={(1/3*100 + "%")} height="100%">
         <Flex wrap={"wrap"}>
           <Heading size="2xl" flexBasis={"100%"}>Hours of Operation:</Heading>
           <Text flexBasis={"100%"}>Sunday - 5:00PM - 12:00AM</Text>
@@ -17,10 +18,10 @@ export function Footer() {
           <Text flexBasis={"100%"}>Saturday - 5:00PM - 4:00AM</Text>
         </Flex>
       </Box>
-      <Box>
-        <Heading>Map Section</Heading>
+      <Box width={(1/3*100 + "%")} height="100%">
+        <Leaflet />
       </Box>
-      <Box>
+      <Box width={(1/3*100 + "%")} height="100%">
         <Heading>Company Information Section</Heading>
       </Box>
     </Flex>
---
+++ src/components/ui/map.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Tooltip,
+} from 'react-leaflet';
+
+// stylesheet imports
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
+const Leaflet = () => {
+  return (
+    <MapContainer
+      style={{ height: "50%", width: "50%" }}
+      center={[42.95539, -85.63196]}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={[42.95539, -85.63196]}>
+        <Tooltip permanent>
+          Eastown Hookah Lounge <br />
+          1522 Wealthy St SE <br />
+          Grand Rapids, MI 49506
+        </Tooltip>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Leaflet;
