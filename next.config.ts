2d4f9f7cc7db71b0fe14a124309060ab65bb8bba
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-03-04 18:09:35 -0500

    Finished footer component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+      compression: "brotli",
+      cacheDirectory: "/.next/cache/webpack",
+      store: "pack"
+    };
+    config.optimization = {
+      ...config.optimization,
+      realContentHash: false
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, List, IconButton, Link } from "@chakra-ui/react";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
 import Leaflet from "./ui/map";
 
 export function Footer() {
@@ -14,7 +15,11 @@ export function Footer() {
       minHeight="500px"
       width="100%" 
       justify={"space-evenly"} 
-      pt={"16"} 
+      pt={{
+        base: 0,
+        sm: 4,
+        lg: 16
+      }} 
       align={"center"} 
       justifyContent={"center"}
       wrap="wrap"
@@ -31,18 +36,35 @@ export function Footer() {
           lg: "100%"
         }}
       >
-        <Flex wrap={"wrap"}>
-          <Heading size="2xl" flexBasis={"100%"}>Hours of Operation:</Heading>
-          <Text flexBasis={"100%"}>Sunday - 5:00PM - 12:00AM</Text>
-          <Text flexBasis={"100%"}>Monday - 5:00PM - 2:00AM</Text>
-          <Text flexBasis={"100%"}>Tuesday - 5:00PM - 2:00AM</Text>
-          <Text flexBasis={"100%"}>Wednesday - 5:00PM - 2:00AM</Text>
-          <Text flexBasis={"100%"}>Thursday - 5:00PM - 2:00AM</Text>
-          <Text flexBasis={"100%"}>Friday - 5:00PM - 4:00AM</Text>
-          <Text flexBasis={"100%"}>Saturday - 5:00PM - 4:00AM</Text>
+        <Flex 
+          wrap={"wrap"} 
+          justify="center"
+          align={"center"}
+          alignContent={"center"}
+          height="100%"
+        >
+          <Heading 
+            size="2xl"
+            flexBasis="100%"
+            textAlign={"center"}
+          >
+            Hours of Operation:
+          </Heading>
+          <List.Root 
+            variant={"plain"}
+            mt="3"
+          >
+            <List.Item>Sunday - 5:00PM - 12:00AM</List.Item>
+            <List.Item>Monday - 5:00PM - 2:00AM</List.Item>
+            <List.Item>Tuesday - 5:00PM - 2:00AM</List.Item>
+            <List.Item>Wednesday - 5:00PM - 2:00AM</List.Item>
+            <List.Item>Thursday - 5:00PM - 2:00AM</List.Item>
+            <List.Item>Friday - 5:00PM - 4:00AM</List.Item>
+            <List.Item>Saturday - 5:00PM - 4:00AM</List.Item>
+          </List.Root>
         </Flex>
       </Box>
-      <Box 
+      <Flex 
         width={{
           base: "100%",
           md: "1/2",
@@ -53,10 +75,11 @@ export function Footer() {
           md: "1/2",
           lg: "100%"
         }}
+        justify={"center"}
       >
         <Leaflet />
-      </Box>
-      <Box 
+      </Flex>
+      <Flex
         width={{
           base: "100%",
           md: "1/2",
@@ -67,9 +90,54 @@ export function Footer() {
           md: "1/2",
           lg: "100%"
         }}
+        align={"center"}
+        alignContent={"center"}
+        justify={{
+          base: "center",
+          lg: "end"
+        }}
+        pr={{
+          base: 4,
+          md: 6,
+          lg:12
+        }}
+        wrap={"wrap"}
       >
-        <Heading>Company Information Section</Heading>
-      </Box>
+        <Heading
+          flexBasis={"100%"}
+          textAlign={"right"}
+          textTransform={"uppercase"}
+        >
+          Eastown Hookah Lounge
+        </Heading>
+        <List.Root
+          variant={"plain"}
+          mt="3"
+        >
+          <List.Item ml={"auto"}>1522 Wealthy St SE, Grand Rapids MI</List.Item>
+          <List.Item ml={"auto"}>Phone: (616) 456-6664</List.Item>
+          <List.Item ml={"auto"}>
+            <IconButton 
+              aria-label="EHL Facebook" 
+              variant={"ghost"}
+              asChild
+            >
+              <Link href="https://www.facebook.com/EastownHookah">
+                <BsFacebook />
+              </Link>
+            </IconButton>
+            <IconButton 
+              aria-label="EHL Instagram"
+              variant={"ghost"}
+              asChild
+            >
+              <Link href="https://www.instagram.com/Eastownhookahlounge">
+                <BsInstagram />
+              </Link>
+            </IconButton>
+          </List.Item>
+        </List.Root>
+      </Flex>
     </Flex>
   )
 }
