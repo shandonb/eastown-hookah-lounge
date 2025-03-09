df9c17ce0cec47a2afdb6c291308696fab2c3bd7
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-03-08 19:38:43 -0500

    Created base menu group component

---
+++ src/components/ui/menu-group.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Separator } from "@chakra-ui/react";
+
+export function MenuGroup () {
+  return (
+    <Separator
+      size={"lg"}
+    />
+    // Headline
+    // Items
+  )
+}
