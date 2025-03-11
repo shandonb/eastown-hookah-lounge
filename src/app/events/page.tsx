883ef2d95a4a646c25a1bd8e33e918053687653e
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-03-10 23:15:41 -0400

    Started layout for event page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,8 +1,15 @@
+import ContentParallax from "@/components/parallax-banner"
 
 export default function EventsPage() {
   return (
     <main>
-      
+      <ContentParallax
+        img="/img/parallax-03.webp"
+        headline="Something New Every Day"
+        copy="With some kind of event on almost every day of the week, you have plenty of vibes to choose from"
+        flexJust="start"
+        textAlign="left"
+      />
     </main>
   )
 }
