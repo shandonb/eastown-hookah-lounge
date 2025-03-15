d1868b61d3e99ea44e07c5fe426f5dc22aa87841
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-03-15 03:17:56 -0400

    Created base calendar component

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,4 +1,5 @@
 import ContentParallax from "@/components/parallax-banner"
+import { Center, Flex, Table } from "@chakra-ui/react"
 
 export default function EventsPage() {
   return (
@@ -10,6 +11,46 @@ export default function EventsPage() {
         flexJust="start"
         textAlign="left"
       />
+      <Center
+        mt={10}
+      >
+        <Flex
+          width={{
+            base: "100%",
+            md: "80%",
+            lg: "50%"
+          }}
+          justify={"center"}
+        >
+          <Table.Root 
+            variant={"outline"}
+            size={"lg"}
+          >
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader padding={"10"}>Sunday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Monday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Tuesday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Wednesday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Thursday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Friday</Table.ColumnHeader>
+                <Table.ColumnHeader padding={"10"}>Saturday</Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell padding={"10"}> - </Table.Cell>
+                <Table.Cell padding={"10"}>Karaoke</Table.Cell>
+                <Table.Cell padding={"10"}> - </Table.Cell>
+                <Table.Cell padding={"10"}>Smokin Spoken Word</Table.Cell>
+                <Table.Cell padding={"10"}>Live DJ</Table.Cell>
+                <Table.Cell padding={"10"}>Live DJ</Table.Cell>
+                <Table.Cell padding={"10"}>Live DJ</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table.Root>
+        </Flex>
+      </Center>
     </main>
   )
 }
---
+++ src/components/calendar.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Flex, Heading } from "@chakra-ui/react";
+import React from "react";
+
+export interface CalendarProps {
+  /** Number of columns. Uses numbers or breakpoints */
+  cols: number | { base?: number, sm?: number, md?: number, lg?: number, xl?: number, "2xl"?: number };
+  /** Calendar headline */
+  title?: string;
+  /** Child object */
+  children: React.ReactNode;
+}
+
+export default function Calendar({cols, children, title}: CalendarProps){
+  let columns: string;
+  let bPointColumns: object;
+  if (typeof cols === "number") {
+    columns = `${100 / cols}%`;
+  } else {
+    bPointColumns = {
+      base: cols.base ? `${100 / cols.base} %` : undefined,
+      sm: cols.sm ? `${100 / cols.sm}%` : undefined,
+      md: cols.md ? `${100 / cols.md}%` : undefined,
+      lg: cols.lg ? `${100 / cols.lg}%` : undefined,
+      xl: cols.xl ? `${100 / cols.xl}%` : undefined,
+      "2xl": cols["2xl"] ? `${100 / cols["2xl"]}%` : undefined,
+    }
+  }
+  return(
+    <>
+      {(title) && (
+        <Flex width="100%">
+          <Heading>{ title }</Heading>
+        </Flex>
+      )}
+      <Flex
+        width={{
+          base: "100%",
+          lg: "60%"
+        }}
+        wrap={"wrap"}
+        justify={"center"}
+      >
+        {React.Children.map(children, (child, index) => (
+          <Flex
+            key={index}
+            wrap={"wrap"}
+            width={ typeof cols === "number" ? columns : bPointColumns }
+          >
+            {child}
+          </Flex>
+        ))}
+      </Flex>
+    </>
+  )
+}
